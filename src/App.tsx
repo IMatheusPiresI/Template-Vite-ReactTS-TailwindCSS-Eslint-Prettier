f56dd850868293b8f2c873ce2afb94e07abc0b6b
imatheuspiresi <matheuspdsousa@gmail.com>
2022-06-23 12:27:34 -0300

    template react projects

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex-center bg-cyan-200">
+      <h1 className="text-5xl font-bold text-cyan-700/75">Ready to Code</h1>
+    </div>
+  );
+}
+
+export default App;
